1f3f968391a3ce6127e651ea39264145c0225398
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-15 16:59:34 -0500

    commit tree 026f1dd082a3871a8f46fc5d521441314892a2d1

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your code here!
+// header
+main.remove()
+let userName='Liz'
+const name =`${userName}`
+const newHeader=document.createElement('div')
+newHeader.id='victory'
+newHeader.textContent=userName+('\'s lists')
+document.body.append(newHeader)
+
+//making a list
+
+
+function createList(listTitle){
+    const newList=document.createElement('ul')
+    newList.textContent=`${listTitle}`
+    newList.className="list"
+    newList.id=`${listTitle.toLowerCase().split(' ').join('-')}`
+    return newList
+}
+
+const toDoList="To Do List"
+const shoppingList="Shopping List"
+const contactList="Contact List"
+
+function addNewList(newList){
+    newHeader.append(createList(newList))
+}
+
+addNewList(toDoList)
+addNewList(shoppingList)
+addNewList(contactList)
+
+// creating items
+
+
+function createTask(listItem){
+    const task=document.createElement('li')
+    task.textContent=`${listItem}`
+    task.className="to-do"
+    return task
+}
+// const practice=createTask("Practice DOM Manipulations")
+// const laundry=createTask("Fold Laundry")
+// const water=createTask("Water Plants")
+
+
+function addToList(listID, task){
+    document.getElementById(listID).append(createTask(task))
+}
+
+addToList('to-do-list', "Practice DOM Manipulations")
+addToList('to-do-list', "Fold Laundry")
+addToList('to-do-list', "Water Plants")
+
+
+
+
+//turn a list item red if completed
+
